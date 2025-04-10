473071f09bcda67b89b39de18fd1dc9bb403e40b
Hubrisy <ahtaras67@gmail.com>
2025-04-10 17:35:32 +0300

    fix: localStorage hook issues [EV-10]

--- src/routes/home/sections/sliderSection/SliderMobile.tsx
+++ src/routes/home/sections/sliderSection/SliderMobile.tsx
@@ -9,59 +9,57 @@ import 'slick-carousel/slick/slick-theme.css';
 
 import Button from '@/components/button';
 
-export const SliderMobile = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
-
-  return (
-    <div className="w-full overflow-hidden">
-      <Slider {...settings}>
-        {sliderProduct.map(item => (
-          <div
-            key={item.id}
-            className={clsx('min-h-[712px] relative w-full flex', item.bg)}
-          >
-            <div className="flex justify-end h-[315px]">
-              <Image
-                style={{ width: item.width }}
-                className={clsx('max-h-[315px]')}
-                width={370}
-                height={390}
-                src={item.img}
-                alt={item.title}
-              />
-            </div>
-            <div className="mt-[38px] max-w-[296px] m-auto flex items-center flex-col">
-              <div
-                className={clsx(
-                  'font-light text-[33px]',
-                  item.isBgBlack ? 'text-white' : 'text-black',
-                )}
-              >
-                {item.title}
-              </div>
-              <div className="mt-large text-gray text-[14px] text-center">
-                {item.description}
-              </div>
-              <Button
-                className={clsx(
-                  'm-[0px] mt-[32px] w-[185px] h-[56px]',
-                  item.isBgBlack
-                    ? 'text-white border-white'
-                    : 'text-black border-black',
-                )}
-              >
-                Shop Now
-              </Button>
-            </div>
-          </div>
-        ))}
-      </Slider>
-    </div>
-  );
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
 };
+
+export const SliderMobile = () => (
+  <div className="w-full overflow-hidden">
+    <Slider {...settings}>
+      {sliderProduct.map(item => (
+        <div
+          key={item.id}
+          className={clsx('min-h-[712px] relative w-full flex', item.bg)}
+        >
+          <div className="flex justify-end h-[315px]">
+            <Image
+              style={{ width: item.width }}
+              className={clsx('max-h-[315px]')}
+              width={370}
+              height={390}
+              src={item.img}
+              alt={item.title}
+            />
+          </div>
+          <div className="mt-[38px] max-w-[296px] m-auto flex items-center flex-col">
+            <div
+              className={clsx(
+                'font-light text-[33px]',
+                item.isBgBlack ? 'text-white' : 'text-black',
+              )}
+            >
+              {item.title}
+            </div>
+            <div className="mt-large text-gray text-[14px] text-center">
+              {item.description}
+            </div>
+            <Button
+              className={clsx(
+                'm-[0px] mt-2xlarge w-[185px] h-[56px]',
+                item.isBgBlack
+                  ? 'text-white border-white'
+                  : 'text-black border-black',
+              )}
+            >
+              Shop Now
+            </Button>
+          </div>
+        </div>
+      ))}
+    </Slider>
+  </div>
+);
--- src/routes/home/sections/sliderSection/index.tsx
+++ src/routes/home/sections/sliderSection/index.tsx
@@ -1,18 +1,13 @@
-import { useEffect, useState } from 'react';
-
 import { SliderDekstop } from './SliderDekstop';
 import { SliderMobile } from './SliderMobile';
 
-export const Slider = () => {
-  const [isMobile, setIsMobile] = useState<boolean>(false);
-
-  useEffect(() => {
-    const checkMobile = () => setIsMobile(window.innerWidth < 640);
-    checkMobile();
-    window.addEventListener('resize', checkMobile);
-
-    return () => window.removeEventListener('resize', checkMobile);
-  }, []);
-
-  return <div>{isMobile ? <SliderMobile /> : <SliderDekstop />}</div>;
-};
+export const Slider = () => (
+  <div>
+    <div className="block sm:hidden">
+      <SliderMobile />
+    </div>
+    <div className="hidden sm:block">
+      <SliderDekstop />
+    </div>
+  </div>
+);
