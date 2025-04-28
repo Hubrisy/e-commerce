ea42e20ee759e104da0a2472c34f26865456148f
Hubrisy <ahtaras67@gmail.com>
2025-04-28 18:30:43 +0300

    feat: added slider to lastseenproducts component [EV-15]

--- src/routes/product/sections/LastVisitedProducts.tsx
+++ src/routes/product/sections/LastVisitedProducts.tsx
@@ -1,8 +1,21 @@
 import { useMemo } from 'react';
+import Slider from 'react-slick';
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 import { ProductItem } from '@/components/productItem';
 import { useProducts } from '@/hooks/use-products';
 import { getStoredVisitedProductIds } from '@/utils/visitedProduct';
+
+const settings = {
+  dots: true,
+  arrows: false,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 2,
+  slidesToScroll: 2,
+};
 
 export const LastVisitedProducts: React.FC = () => {
   const { products } = useProducts();
@@ -22,14 +35,16 @@ export const LastVisitedProducts: React.FC = () => {
   }, [products?.length]);
 
   return (
-    <div className="my-14">
+    <div className="mt-14 mb-22">
       <div className="text-2xl font-medium">Last seen products</div>
-      <div className="mt-2xlarge grid grid-cols-2 gap-4">
-        {lastSeenProducts?.map(product => (
-          <div key={product.id}>
-            <ProductItem product={product} />
-          </div>
-        ))}
+      <div className="mt-2xlarge">
+        <Slider {...settings} className="last-seen_products">
+          {lastSeenProducts?.map(product => (
+            <div key={product.id}>
+              <ProductItem product={product} />
+            </div>
+          ))}
+        </Slider>
       </div>
     </div>
   );
