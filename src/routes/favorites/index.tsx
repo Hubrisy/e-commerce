3bd018bb6ed78bd6168b640b0e2e8f418d3730a2
Hubrisy <ahtaras67@gmail.com>
2025-07-10 16:59:28 +0300

    fix: fixed responsive design for favorites component [EV-00]

--- src/routes/favorites/index.tsx
+++ src/routes/favorites/index.tsx
@@ -11,12 +11,12 @@ const Favorites = () => {
   );
 
   return (
-    <div className="max-w-[1100px] mb-3xlarge m-auto">
-      <div className="mt-[128px] max-w-[340px] m-auto sm:m-0 sm:ml-10 md:mt-[72px] xl:ml-0">
+    <div className="max-w-[1100px] mb-3xlarge m-auto mt-[128px] md:mt-[72px]">
+      <div className="m-auto sm:m-0 xl:ml-0">
         <div className="text-2xl font-semibold">Your favorites:</div>
-        <div className="grid grid-cols-2 gap-2 mt-4">
+        <div className="grid grid-cols-2 gap-4 mt-4 md:grid-cols-3 lg:grid-cols-4">
           {arrayOfFavorites?.map(item => (
-            <div key={item.id}>
+            <div key={item.id} className="max-w-[250px]">
               <ProductItem product={item} />
             </div>
           ))}
