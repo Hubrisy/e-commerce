ae3a0f47ef1f77a730edd003dd0ecc857910b9a2
Hubrisy <ahtaras67@gmail.com>
2025-07-10 16:27:17 +0300

    feat: added responsive design for favorites page [EV-00]

--- src/routes/favorites/index.tsx
+++ src/routes/favorites/index.tsx
@@ -11,14 +11,16 @@ const Favorites = () => {
   );
 
   return (
-    <div className="mt-[128px] mb-3xlarge max-w-[340px] m-auto">
-      <div className="text-2xl font-semibold">Your favorites:</div>
-      <div className="grid grid-cols-2 gap-2 mt-4">
-        {arrayOfFavorites?.map(item => (
-          <div key={item.id}>
-            <ProductItem product={item} />
-          </div>
-        ))}
+    <div className="max-w-[1100px] mb-3xlarge m-auto">
+      <div className="mt-[128px] max-w-[340px] m-auto sm:m-0 sm:ml-10 md:mt-[72px] xl:ml-0">
+        <div className="text-2xl font-semibold">Your favorites:</div>
+        <div className="grid grid-cols-2 gap-2 mt-4">
+          {arrayOfFavorites?.map(item => (
+            <div key={item.id}>
+              <ProductItem product={item} />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
